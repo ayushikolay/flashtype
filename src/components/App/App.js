25866619ecd7fa67dd726c3c06052fa78798ece9
Ayushi Kolay <ayushi18071998@gmail.com>
2021-12-26 17:59:46 +0530

    Completed Navbar Component

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./../Navbar/Navbar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        {/*** Nav Section*/}
+        <Navbar />
+
+        {/* Landing Page  */}
+
+        {/* Challenge section  */}
+
+        {/* Footer  */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import logo from "././../../assets/logo.png";
+import "./Navbar.css";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="nav-container">
+          <div className="nav-left">
+            <img className="flash-logo" src={logo} alt="logo" />
+            <p className="flash-logo-text">flashType</p>
+          </div>
+          <div className="nav-right">
+            <a
+              target="_blank"
+              className="nav-aam-link"
+              href="https://github.com/ayushikolay/flashtype"
+              rel="noreferrer"
+            >
+              Source Code
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
